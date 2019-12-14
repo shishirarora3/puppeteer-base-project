ca58504764a08ce555f5a1c56084af322706c680
shishir arora <shiarora@microsoft.com>
2019-12-15 04:35:04 +0530

    first commit

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # puppeteer-base-project
 Simple base project showing puppeteer.
-
+and a method to test without login
 ```
 npm install
 
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222
+http://127.0.0.1:9222/json
+copy webSocketDebuggerUrl and replace in example.js
 node example.js
 ```
--- example.js
+++ example.js
@@ -3,46 +3,33 @@ let fs = require('fs');
 
 (async () => {
     // OPTION 1 - Launch new.
-    const browser = await puppeteer.launch({
+    /**const browser = await puppeteer.launch({
         headless: false // Puppeteer is 'headless' by default.
-    });
+    });**/
 
     // OPTION 2 - Connect to existing.
     // MAC: /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
     // PC: start chrome.exe –remote-debugging-port=9222
     // Note: this url changes each time the command is run.
-    // const wsChromeEndpointUrl = 'ws://localhost:9223/devtools/page/BA7D54678E00FDA621034A1E51367601';
-    // const browser = await puppeteer.connect({
-    //     browserWSEndpoint: wsChromeEndpointUrl
-    // });
+     const wsChromeEndpointUrl = 'ws://127.0.0.1:9222/devtools/page/2EC769F4CFCE9810279B82D8A29DAD7A';
+     const browser = await puppeteer.connect({
+         browserWSEndpoint: wsChromeEndpointUrl
+     });
 
     const page = await browser.newPage();
-    let pageUrl = 'https://caniuse.com/';
-
-    await page.goto(pageUrl, {
-        waitUntil: 'networkidle0' // 'networkidle0' is very useful for SPAs.
-    });
-
-    let mostSearchedList = await page.evaluate(() => {
-        let objectList = document.querySelectorAll('.js-most-searched .home__list-item');
-        let mostSearched = [];
-
-        objectList.forEach((item) => {
-            let child = item.firstChild;
-            let title = child.innerText;
-            let href = child.href;
-
-            mostSearched.push(title + ' - ' + href);
-        });
-
-        return mostSearched;
-    });
-
-    fs.writeFile("mostSearched.txt", mostSearchedList, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    
-        console.log("The file was saved!");
-    });     
+    await page.goto('https://outlook-sdf.office.com/calendar/view/agenda?gulp&culture=en')
+  
+    await page.waitForSelector('.agenda-item-wrapper > .CalendarSurfaceItem---itemCardWrapper---2NNHc:nth-child(2) > .content-76 > .CalendarInDaySurfaceItem---inDayContentWrapper---10N5m > .CalendarInDaySurfaceItem---itemContent---2K2XW')
+  await page.click('.agenda-item-wrapper > .CalendarSurfaceItem---itemCardWrapper---2NNHc:nth-child(2) > .content-76 > .CalendarInDaySurfaceItem---inDayContentWrapper---10N5m > .CalendarInDaySurfaceItem---itemContent---2K2XW')
+  
+  await page.waitForSelector('.CalendarReadingPaneBody---readingPane---bqc7t > .HubSection---hubSection---kEGHc > .MeetingResponseInfo---itemContainer---1iyrL > .HubItem---additionalInfoContainer---2KUdw > .Button---button---3wSGW')
+  await page.click('.CalendarReadingPaneBody---readingPane---bqc7t > .HubSection---hubSection---kEGHc > .MeetingResponseInfo---itemContainer---1iyrL > .HubItem---additionalInfoContainer---2KUdw > .Button---button---3wSGW')
+  
+  await page.waitForSelector('.BottomSheet---bottomSheetContainer---3L8X7 > .SelectableList---selectableList---bhnlH > .HubItem---hubItemContainer---1RaMQ:nth-child(1) > .HubItem---textContainer---2re5a > .SelectableList---title---1j7ad')
+  await page.click('.BottomSheet---bottomSheetContainer---3L8X7 > .SelectableList---selectableList---bhnlH > .HubItem---hubItemContainer---1RaMQ:nth-child(1) > .HubItem---textContainer---2re5a > .SelectableList---title---1j7ad')
+  
+  await page.waitForSelector('.RsvpActions---rsvpActionsContainer---3cmet > .HubItem---hubItemContainer---1RaMQ > .HubItem---additionalInfoContainer---2KUdw > .Toggle---buttonContainer---3wTNW > .Toggle---checkedPill---2eKac')
+  await page.click('.RsvpActions---rsvpActionsContainer---3cmet > .HubItem---hubItemContainer---1RaMQ > .HubItem---additionalInfoContainer---2KUdw > .Toggle---buttonContainer---3wTNW > .Toggle---checkedPill---2eKac')
+  
+          
 })();
